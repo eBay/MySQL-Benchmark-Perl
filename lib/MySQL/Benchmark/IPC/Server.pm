81a8576c66291581ff0849b5fcfddfce03f7a557
Luis Motta Campos <lucampos@ebay.com>
2014-02-27 18:59:17 +0000

    Applying ebay-approved GPLv2 License
    Follwing the instructions from Mark Yuan, applying the GPLv2 License
    ebay granted me as the last preparation step before publishing this tool
    as open-source.

--- lib/MySQL/Benchmark/IPC/Server.pm
+++ lib/MySQL/Benchmark/IPC/Server.pm
@@ -81,24 +81,27 @@ sub receive {
 
 =head1 AUTHOR
 
-Luis Motta Campos, C<< <lmc at cpan.org> >>
+Luis Motta Campos, C<< <lucampos at ebay.com> >>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2013 Luis Motta Campos.
+Copyright (C) 2013-2014 eBay Software Foundation
 
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation; version 2 dated June, 1991 or at your option any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-A copy of the GNU General Public License is available in the source tree; if
-not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 =cut
+
 
 1;    # End of MySQL::Benchmark::IPC::Server
